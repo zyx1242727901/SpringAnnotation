a276f86acc12c4f5f6ccaa4f8d2ad1779b9cdcf6
zhangyuxiao <zhangyuxiao@daokoudai.com>
2020-07-06 14:51:59 +0800

    create project

---
+++ demo-project/src/main/java/com/zyx/demo/TestMain.java
@@ -0,0 +1,7 @@
+package com.zyx.demo;
+
+public class TestMain {
+	public static void main(String[] args) {
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+	}
+}
