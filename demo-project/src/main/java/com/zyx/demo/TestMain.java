af11599def0f34fd3c7d2c4a19be492360582404
zhangyuxiao <zhangyuxiao@daokoudai.com>
2020-07-06 16:24:23 +0800

    gradle depencies update

--- demo-project/src/main/java/com/zyx/demo/TestMain.java
+++ demo-project/src/main/java/com/zyx/demo/TestMain.java
@@ -1,7 +1,23 @@
 package com.zyx.demo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Configuration
 public class TestMain {
+
+	@RequestMapping("/test")
+	public String test(){
+		return "success";
+	}
+
 	public static void main(String[] args) {
-//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestMain.class);
+		TestMain test = (TestMain) context.getBean("test");
+
 	}
 }
